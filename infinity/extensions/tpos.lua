c8d130f97d6a55397d6fc4746ba6298c0548b4ef
benarc <ben@arc.wales>
2021-11-07 15:53:46 +0000

    Added bracket

--- infinity/extensions/tpos.lua
+++ infinity/extensions/tpos.lua
@@ -1,7 +1,9 @@
 models = {
+  {
     name = 'tpos',
     fields = {
       {name = 'title', required = true, type = 'string'},
       {name = 'currency', required = true, type = 'string'},
     }
+  }
 }
